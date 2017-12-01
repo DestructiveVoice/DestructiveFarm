b523a25c4429403126b332bf713c55a2ff864ea8
Alexander Borzunov <hxrussia@gmail.com>
2017-12-02 04:35:28 +0500

    Improve the main page

--- server/views.py
+++ server/views.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
 from flask import render_template
 
 from server import app, database
+
+
+@app.template_filter('timestamp_to_datetime')
+def timestamp_to_datetime(s):
+    return datetime.fromtimestamp(s)
 
 
 @app.route('/')
