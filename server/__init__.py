e32c5abf200658bf52fb872842705e086f3454de
Alexander Borzunov <hxrussia@gmail.com>
2018-04-11 02:12:44 +0500

    Add start_server.sh script

--- server/__init__.py
+++ server/__init__.py
@@ -15,7 +15,3 @@ import server.views
 if not werkzeug.serving.is_running_from_reloader():
     threading.Thread(target=server.submit_loop.run_loop, daemon=True).start()
     # FIXME: Don't use daemon=True, exit from the thread properly
-
-
-# if __name__ == '__main__':
-#     app.run(threaded=True, debug=True)
---
+++ server/start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Use FLASK_DEBUG=True if needed
+
+FLASK_APP=__init__.py flask run --with-threads
