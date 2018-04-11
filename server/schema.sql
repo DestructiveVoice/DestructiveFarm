135a3aa32bb9141a29ae3e33751c7dcdf00437d6
Alexander Borzunov <hxrussia@gmail.com>
2018-04-11 10:52:56 +0500

    Start creating the front-end for the farm server

--- server/schema.sql
+++ server/schema.sql
@@ -7,5 +7,7 @@ CREATE TABLE IF NOT EXISTS flags (
     checksystem_response TEXT
 );
 
+CREATE INDEX IF NOT EXISTS flags_sploit ON flags(sploit);
+CREATE INDEX IF NOT EXISTS flags_team ON flags(team);
 CREATE INDEX IF NOT EXISTS flags_status_time ON flags(status, time);
 CREATE INDEX IF NOT EXISTS flags_time ON flags(time);
--- server/views.py
+++ server/views.py
@@ -1,9 +1,9 @@
 import time
 from datetime import datetime
 
-from flask import render_template, request
+from flask import jsonify, render_template, request
 
-from server import app, database
+from server import app, database, reloader
 
 
 @app.template_filter('timestamp_to_datetime')
@@ -13,33 +13,56 @@ def timestamp_to_datetime(s):
 
 @app.route('/')
 def index():
-    start_time = time.time()
+    distinct_values = {}
+    for column in ['sploit', 'status', 'team']:
+        rows = database.query('SELECT DISTINCT {} FROM flags ORDER BY {}'.format(column, column))
+        distinct_values[column] = [item[column] for item in rows]
 
-    query = 'SELECT * FROM flags'
+    config = reloader.get_config()
+
+    server_tz_name = time.strftime('%Z')
+    if server_tz_name.startswith('+'):
+        server_tz_name = 'UTC' + server_tz_name
+
+    return render_template('index.html',
+                           flag_format=config['FLAG_FORMAT'],
+                           distinct_values=distinct_values,
+                           server_tz_name=server_tz_name)
+
+
+FORM_DATETIME_FORMAT = '%Y-%m-%d %H:%M'
+FLAGS_PER_PAGE = 30
+
+
+@app.route('/ui/show_flags', methods=['POST'])
+def show_flags():
     conditions = []
+    for column in ['sploit', 'status', 'team']:
+        value = request.form[column]
+        if value:
+            conditions.append(('{} = ?'.format(column), value))
+    for column in ['flag', 'checksystem-response']:
+        value = request.form[column]
+        if value:
+            conditions.append(('INSTR(LOWER({}), ?)'.format(column), value))
+    for param in ['time-since', 'time-until']:
+        value = request.form['time-since'].strip()
+        if value:
+            timestamp = round(datetime.strptime(value, FORM_DATETIME_FORMAT).timestamp())
+            conditions.append(('time >= ?' if param == 'time-since' else 'time-until <= ?', timestamp))
+    page_number = int(request.form['page-number'])
+    if page_number < 1:
+        raise ValueError('Invalid page-number')
+
+    sql = 'SELECT * FROM flags'
     args = []
-
-    for column in ['sploit', 'team', 'status']:
-        if column in request.args:
-            conditions.append(column + ' = ?')
-            args.append(request.args[column])
-
-    if 'last-minutes' in request.args:
-        min_time = round(time.time() - int(request.args['last-minutes']) * 60)
-        conditions.append('time >= ?')
-        args.append(min_time)
-
     if conditions:
-        query += ' WHERE ' + ' AND '.join(conditions)
-    query += ' ORDER BY time DESC'
+        chunks, values = list(zip(conditions))
+        sql += ' WHERE ' + ' AND '.join(chunks)
+        args += values
+    sql += ' ORDER BY time DESC LIMIT ? OFFSET ?'
+    args += [FLAGS_PER_PAGE, FLAGS_PER_PAGE * (page_number - 1)]
 
-    query += ' LIMIT ?'
-    limit = int(request.args.get('limit', 100))
-    args.append(limit)
+    flags = database.query(sql, args)
 
-    flags = database.query(query, args)
-
-    gen_time = time.time() - start_time
-    return render_template('index.html', flags=flags,
-                           limit=limit, has_conditions=bool(conditions),
-                           gen_time=gen_time)
+    return jsonify([dict(item) for item in flags])
