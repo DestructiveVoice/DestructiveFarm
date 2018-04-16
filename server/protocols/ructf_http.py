31dd980d98653cfac0d49c344a042b35b009fc8b
Alexander Borzunov <hxrussia@gmail.com>
2018-04-17 02:51:36 +0500

    Fix protocols for RuCTF checksystem ("no such flag" verdict and ructf_http.py)

--- server/protocols/ructf_http.py
+++ server/protocols/ructf_http.py
@@ -5,11 +5,18 @@ from server.models import FlagStatus, SubmitResult
 
 
 RESPONSES = {
-    FlagStatus.QUEUED: ['timeout', 'game not started', 'try again later', 'game over', 'is not up'],
+    FlagStatus.QUEUED: ['timeout', 'game not started', 'try again later', 'game over', 'is not up',
+                        'no such flag'],
     FlagStatus.ACCEPTED: ['accepted', 'congrat'],
-    FlagStatus.REJECTED: ['bad', 'wrong', 'expired', 'unknown', 'your own', 'no such flag',
+    FlagStatus.REJECTED: ['bad', 'wrong', 'expired', 'unknown', 'your own',
                           'too old', 'not in database', 'already submitted', 'invalid flag'],
 }
+# The RuCTF checksystem adds a signature to all correct flags. It returns
+# "invalid flag" verdict if the signature is invalid and "no such flag" verdict if
+# the signature is correct but the flag was not found in the checksystem database.
+#
+# The latter situation happens if a checker puts the flag to the service before putting it
+# to the checksystem database. We should resent the flag later in this case.
 
 
 TIMEOUT = 5
@@ -18,15 +25,16 @@ TIMEOUT = 5
 def submit_flags(flags, config):
     r = requests.put(config['SYSTEM_URL'],
                      headers={'X-Team-Token': config['SYSTEM_TOKEN']},
-                     json=[item.flag.encode() for item in flags], timeout=TIMEOUT)
+                     json=[item.flag for item in flags], timeout=TIMEOUT)
 
     unknown_responses = set()
     for item in r.json():
-        response = item['msg'].strip.lower()
-        response = response.replace('[{}] '.format(item.flag), '')
+        response = item['msg'].strip()
+        response = response.replace('[{}] '.format(item['flag']), '')
 
+        response_lower = response.lower()
         for status, substrings in RESPONSES.items():
-            if any(s in response for s in substrings):
+            if any(s in response_lower for s in substrings):
                 found_status = status
                 break
         else:
--- server/protocols/ructf_tcp.py
+++ server/protocols/ructf_tcp.py
@@ -5,12 +5,18 @@ from server.models import FlagStatus, SubmitResult
 
 
 RESPONSES = {
-    FlagStatus.QUEUED: ['timeout', 'game not started', 'try again later', 'game over', 'is not up'],
+    FlagStatus.QUEUED: ['timeout', 'game not started', 'try again later', 'game over', 'is not up',
+                        'no such flag'],
     FlagStatus.ACCEPTED: ['accepted', 'congrat'],
-    FlagStatus.REJECTED: ['bad', 'wrong', 'expired', 'unknown', 'your own', 'no such flag',
+    FlagStatus.REJECTED: ['bad', 'wrong', 'expired', 'unknown', 'your own',
                           'too old', 'not in database', 'already submitted', 'invalid flag'],
 }
-
+# The RuCTF checksystem adds a signature to all correct flags. It returns
+# "invalid flag" verdict if the signature is invalid and "no such flag" verdict if
+# the signature is correct but the flag was not found in the checksystem database.
+#
+# The latter situation happens if a checker puts the flag to the service before putting it
+# to the checksystem database. We should resent the flag later in this case.
 
 READ_TIMEOUT = 5
 APPEND_TIMEOUT = 0.05
