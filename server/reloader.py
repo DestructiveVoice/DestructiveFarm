9c3a8753fd8e53629ad0a8f6f74784de5b314431
Nikolay Amiantov <ab@fmap.me>
2020-11-14 21:23:38 +0300

    Allow config path to be specified

--- server/reloader.py
+++ server/reloader.py
@@ -1,13 +1,22 @@
 import importlib
 import os
 import threading
+import importlib.util
 
-from server import app, config as config_module
+from server import app
 
 
 _config_mtime = None
 _reload_lock = threading.RLock()
 
+if "CONFIG" in os.environ:
+    config_path = os.environ["CONFIG"]
+else:
+    config_path = os.path.join(app.root_path, 'config.py')
+
+config_spec = importlib.util.spec_from_file_location("server.config", config_path)
+config_module = importlib.util.module_from_spec(config_spec)
+config_spec.loader.exec_module(config_module)
 _cur_config = config_module.CONFIG
 
 
@@ -24,12 +33,12 @@ def get_config():
 
     global _config_mtime, _cur_config
 
-    cur_mtime = os.stat(os.path.join(app.root_path, 'config.py')).st_mtime_ns
+    cur_mtime = os.stat(config_path).st_mtime_ns
     if cur_mtime != _config_mtime:
         with _reload_lock:
             if cur_mtime != _config_mtime:
                 try:
-                    importlib.reload(config_module)
+                    config_spec.loader.exec_module(config_module)
                     _cur_config = config_module.CONFIG
                     app.logger.info('New config loaded')
                 except Exception as e:
