69cc5821a16fc38e6670e666bc0c8d5ee311e57a
vladko312 <37411315+vladko312@users.noreply.github.com>
2022-03-17 16:19:26 +0300

    Make start_server.sh work if run from another directory (#24)

--- server/start_server.sh
+++ server/start_server.sh
@@ -2,4 +2,4 @@
 
 # Use FLASK_DEBUG=True if needed
 
-FLASK_APP=standalone.py python3 -m flask run --host 0.0.0.0 --with-threads
+FLASK_APP=$(dirname $(readlink -f $0))/standalone.py python3 -m flask run --host 0.0.0.0 --with-threads
